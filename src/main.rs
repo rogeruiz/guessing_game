6f34a7dc488f1d3cab8e7cc99a2076262d6c8a33
Roger Steve Ruiz <hi@rog.gr>
2016-06-11 00:21:53 -0400

    Add compare ordering & learn about static types;
    ```
       Compiling guessing_game v0.1.0 (file:///Users/rogerruiz/Developer/open-source/rust-pg/guessing_game)
    src/main.rs:24:22: 24:36 error: mismatched types:
     expected `&std::string::String`,
        found `&_`
    (expected struct `std::string::String`,
        found integral variable) [E0308]
    src/main.rs:24     match guess.cmp( &secret_number ) {
                                        ^~~~~~~~~~~~~~
    src/main.rs:24:22: 24:36 help: run `rustc --explain E0308` to see a detailed explanation
    error: aborting due to previous error
    error: Could not compile `guessing_game`.
    
    To learn more, run the command again with --verbose.
    
    shell returned 101
    ```

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate rand;
 
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
@@ -19,5 +20,11 @@ fn main() {
         .expect( "Faild to read line!" );
 
     println!( "You guessed: {}", guess );
+
+    match guess.cmp( &secret_number ) {
+        Ordering::Less => println!( "Too small of a guess!" ),
+        Ordering::Greater => println!( "Too big!" ),
+        Ordering::Equal => println!( "You win!" ),
+    }
 
 }
