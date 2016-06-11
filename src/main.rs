1bae37b7e2aa0965f06fc07bc31c012c1291d7b4
Roger Steve Ruiz <hi@rog.gr>
2016-06-11 00:05:02 -0400

    Start playing around with i/o

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
+use std::io;
+
 fn main() {
 
     println!( "Hello World" );
+
+    println!( "Please input your guess." );
+
+    let mut guess = String::new();
+
+    io::stdin().read_line( &mut guess )
+        .expect( "Faild to read line!" );
+
+    println!( "You guessed: {}", guess );
 
 }
