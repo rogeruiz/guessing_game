f71c0a24f5c46111958665b895567e8789aba1df
Roger Steve Ruiz <hi@rog.gr>
2016-06-11 00:29:30 -0400

    Add an infinite loop!

--- src/main.rs
+++ src/main.rs
@@ -12,22 +12,26 @@ fn main() {
 
     println!( "The secret number is: {}", secret_number );
 
-    println!( "Please input your guess." );
+    loop {
 
-    let mut guess = String::new();
+        println!( "Please input your guess." );
 
-    io::stdin().read_line( &mut guess )
-        .expect( "Faild to read line!" );
+        let mut guess = String::new();
 
-    let guess: u32 = guess.trim().parse()
-        .expect( "Please type a number!" );
+        io::stdin().read_line( &mut guess )
+            .expect( "Faild to read line!" );
 
-    println!( "You guessed: {}", guess );
+        let guess: u32 = guess.trim().parse()
+            .expect( "Please type a number!" );
 
-    match guess.cmp( &secret_number ) {
-        Ordering::Less => println!( "Too small of a guess!" ),
-        Ordering::Greater => println!( "Too big of a guess!" ),
-        Ordering::Equal => println!( "You win!" ),
+        println!( "You guessed: {}", guess );
+
+        match guess.cmp( &secret_number ) {
+            Ordering::Less => println!( "Too small of a guess!" ),
+            Ordering::Greater => println!( "Too big of a guess!" ),
+            Ordering::Equal => println!( "You win!" ),
+        }
+
     }
 
 }
