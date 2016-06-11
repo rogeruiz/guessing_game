2162fd3daf4ff0a43eb0a55716e0fe9ba9b98ab2
Roger Steve Ruiz <hi@rog.gr>
2016-06-11 00:26:26 -0400

    Do a type conversion;
    https://images.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.aircraftrecognition.co.uk%2Fimages%2FAerobatics%2FAerobatics%2520-%2520Barrell%2520Roll.png&f=1

--- src/main.rs
+++ src/main.rs
@@ -19,11 +19,14 @@ fn main() {
     io::stdin().read_line( &mut guess )
         .expect( "Faild to read line!" );
 
+    let guess: u32 = guess.trim().parse()
+        .expect( "Please type a number!" );
+
     println!( "You guessed: {}", guess );
 
     match guess.cmp( &secret_number ) {
         Ordering::Less => println!( "Too small of a guess!" ),
-        Ordering::Greater => println!( "Too big!" ),
+        Ordering::Greater => println!( "Too big of a guess!" ),
         Ordering::Equal => println!( "You win!" ),
     }
 
