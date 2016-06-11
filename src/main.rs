5fa9f552d3c96ce99a540e1cb2f860ba5e3665d5
Roger Steve Ruiz <hi@rog.gr>
2016-06-11 00:32:29 -0400

    Break on win & don't bail on the number party

--- src/main.rs
+++ src/main.rs
@@ -21,15 +21,20 @@ fn main() {
         io::stdin().read_line( &mut guess )
             .expect( "Faild to read line!" );
 
-        let guess: u32 = guess.trim().parse()
-            .expect( "Please type a number!" );
+        let guess: u32 = match guess.trim().parse() {
+            Ok( num ) => num,
+            Err( _ ) => continue,
+        };
 
         println!( "You guessed: {}", guess );
 
         match guess.cmp( &secret_number ) {
             Ordering::Less => println!( "Too small of a guess!" ),
             Ordering::Greater => println!( "Too big of a guess!" ),
-            Ordering::Equal => println!( "You win!" ),
+            Ordering::Equal => {
+                println!( "You win!" );
+                break;
+            },
         }
 
     }
