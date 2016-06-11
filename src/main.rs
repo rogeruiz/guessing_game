a526a0dbe7601cb160c80c6a81c3f2159ab4e13f
Roger Steve Ruiz <hi@rog.gr>
2016-06-11 00:27:07 -0400

    Fix typo

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range( 1, 101 );
 
-    println!( "The secret numebr is: {}", secret_number );
+    println!( "The secret number is: {}", secret_number );
 
     println!( "Please input your guess." );
 
