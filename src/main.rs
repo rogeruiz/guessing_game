fd854a8ee8c97d11f5be5a8c9a2e5cfa57417c41
Roger Steve Ruiz <hi@rog.gr>
2016-06-11 00:17:34 -0400

    Add rand and build a random number generator

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
+extern crate rand;
+
 use std::io;
+use rand::Rng;
 
 fn main() {
 
-    println!( "Hello World" );
+    println!( "Guess the number!" );
+
+    let secret_number = rand::thread_rng().gen_range( 1, 101 );
+
+    println!( "The secret numebr is: {}", secret_number );
 
     println!( "Please input your guess." );
 
